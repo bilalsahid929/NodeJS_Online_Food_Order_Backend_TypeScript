484233b858156223ce0ad69135f5743209a5dd20
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-20 15:20:11 +0400

    update the file path

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -4,17 +4,18 @@ import path from "path";
 import {
   AdminRoute,
   ShoppingRoute,
-  VandorRoute,
+  VendorRoute,
   CustomerRoute,
 } from "../routes";
 
 export default async (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/images", express.static(path.join(__dirname, "images")));
+  const imagePath = path.join(__dirname, "../images");
+  app.use("/images", express.static(imagePath));
 
   app.use("/admin", AdminRoute);
-  app.use("/vandor", VandorRoute);
+  app.use("/vendor", VendorRoute);
   app.use("/customer", CustomerRoute);
   app.use("/", ShoppingRoute);
 
